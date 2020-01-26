e972536b2f3972db59200128682692de4daf43b7
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 15:50:11 +0100

    Finally implemented IUnitOfWork in UnitOfWork Class

--- FindMyRestaurant/Infrastructure/UnitOfWork.cs
+++ FindMyRestaurant/Infrastructure/UnitOfWork.cs
@@ -1,11 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using FindMyRestaurant.Core;
+using FindMyRestaurant.Infrastructure.Data;
 
 namespace FindMyRestaurant.Infrastructure
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+
+        #region Fields
+        private readonly AppDbContext _context;
+        #endregion
+
+        #region Constructor
+        public UnitOfWork()
+        {
+            _context = new AppDbContext();
+        }
+        #endregion
+
+
+        #region Members
+        #endregion
+
+        #region Methods
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async void SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Disposable Member
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+        #endregion
     }
 }

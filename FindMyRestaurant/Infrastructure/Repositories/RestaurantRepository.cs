26aaacae512099e8525861bd1fc1475b992fd301
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 20:21:22 +0100

    Implement IRestaurantRepository in RestaurantRepository

---
+++ FindMyRestaurant/Infrastructure/Repositories/RestaurantRepository.cs
@@ -0,0 +1,15 @@
+﻿using FindMyRestaurant.Core.Domain;
+using FindMyRestaurant.Core.Repositories;
+using FindMyRestaurant.Infrastructure.Data;
+
+namespace FindMyRestaurant.Infrastructure.Repositories
+{
+    public class RestaurantRepository : Repository<Restaurant>, IRestaurantRepository
+    {
+        public RestaurantRepository(AppDbContext context)
+            : base(context)
+        {
+        }
+
+    }
+}

24627f87cdd32b8163a24eb275216ff7d0bceb02
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 15:39:40 +0100

    Add Methods to Interface IRepository

--- FindMyRestaurant/Core/Repositories/IRepository.cs
+++ FindMyRestaurant/Core/Repositories/IRepository.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace FindMyRestaurant.Core.Repositories
 {
-    interface IRepository
+    public interface IRepository<TEntity> where TEntity : class
     {
+        List<TEntity> GetAll();
+        Task<List<TEntity>> GetAllAsync();
+
+
+        TEntity FindById(object id);
+        Task<TEntity> FindByIdAsync(object id);
+
+
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
     }
 }

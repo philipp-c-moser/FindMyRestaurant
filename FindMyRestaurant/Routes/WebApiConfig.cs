7e5c72c26f7375d100ca2f8ff2e70bbe1a16c1a3
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 16:33:22 +0100

    Move RouteConfigurationFiles to Subfolder "Routes"

--- FindMyRestaurant/Routes/WebApiConfig.cs
+++ FindMyRestaurant/Routes/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
-namespace FindMyRestaurant
+namespace FindMyRestaurant.Routes
 {
     public static class WebApiConfig
     {

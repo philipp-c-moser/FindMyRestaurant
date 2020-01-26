628166c85bee280ca2cb12a296ba3ebe11556760
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 15:35:22 +0100

    Add AppDbContext Class

---
+++ FindMyRestaurant/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace FindMyRestaurant.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+
+        public AppDbContext()
+            : base("name=DefaultConnection")
+        {
+        }
+
+        public static AppDbContext Create()
+        {
+            return new AppDbContext();
+        }
+
+    }
+}

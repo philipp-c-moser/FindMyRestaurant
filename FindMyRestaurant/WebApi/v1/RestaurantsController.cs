436365870a85700a9eb36f94f8ae5a356dcff262
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 21:43:02 +0100

    Add new Api Controller => RestaurantsController

---
+++ FindMyRestaurant/WebApi/v1/RestaurantsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+using FindMyRestaurant.Framework.Controllers;
+using FindMyRestaurant.Core;
+
+namespace FindMyRestaurant.WebApi.v1
+{
+    [RoutePrefix("Api/V1/Restaurants")]
+    public class RestaurantsController : ApiControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RestaurantsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}

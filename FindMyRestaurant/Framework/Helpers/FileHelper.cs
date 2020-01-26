d3aebff9560d98e25805c805ba5976e2d7d60dfe
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 15:11:39 +0100

    New Framework section and FileHelper Class.
    Method BuildImageUrl builds the absolute image url

---
+++ FindMyRestaurant/Framework/Helpers/FileHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindMyRestaurant.Framework.Helpers
+{
+    public static class FileHelper
+    {
+
+        #region PublicMethods
+        public static string BuildImageUrl(string imageName, string fileType)
+        {
+            return "https://cnt.01.bb.delivery.philipp-moser.de/irfdoe2un/" + imageName + "." + fileType;
+        }
+        #endregion
+
+
+        #region PrivateMethods
+        #endregion
+
+
+
+
+    }
+}

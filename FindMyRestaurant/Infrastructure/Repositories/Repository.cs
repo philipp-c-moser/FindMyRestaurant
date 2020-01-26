45126a5ed6396e5ffeea63e3ff9666da0c31f727
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 15:44:01 +0100

    Implementing IRepository in Repository Class

--- FindMyRestaurant/Infrastructure/Repositories/Repository.cs
+++ FindMyRestaurant/Infrastructure/Repositories/Repository.cs
@@ -1,11 +1,80 @@
-﻿using System;
+﻿using System.Data.Entity;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Linq;
-using System.Web;
+using FindMyRestaurant.Infrastructure.Data;
+using FindMyRestaurant.Core.Repositories;
 
 namespace FindMyRestaurant.Infrastructure.Repositories
 {
-    public class Repository
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
+        private AppDbContext _context;
+        private DbSet<TEntity> _set;
+
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        protected DbSet<TEntity> Set
+        {
+            get
+            {
+                return _set ?? (_set = _context.Set<TEntity>());
+            }
+        }
+
+
+
+
+
+        public List<TEntity> GetAll()
+        {
+            return Set.ToList();
+        }
+
+        public Task<List<TEntity>> GetAllAsync()
+        {
+            return Set.ToListAsync();
+        }
+
+
+
+        public TEntity FindById(object id)
+        {
+            return Set.Find(id);
+        }
+
+        public Task<TEntity> FindByIdAsync(object id)
+        {
+            return Set.FindAsync(id);
+        }
+
+
+
+        public void Add(TEntity entity)
+        {
+            Set.Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            var entry = _context.Entry(entity);
+            if (entry.State == EntityState.Detached)
+            {
+                Set.Attach(entity);
+                entry = _context.Entry(entity);
+            }
+
+            entry.State = EntityState.Modified;
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Set.Remove(entity);
+        }
+
     }
 }

3fe3b73ff21941b8c29593b9e35470278ebdd8b3
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 20:43:31 +0100

    Add new RestaurantCategoryRepository

---
+++ FindMyRestaurant/Infrastructure/Repositories/RestaurantCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using FindMyRestaurant.Core.Domain;
+using FindMyRestaurant.Core.Repositories;
+using FindMyRestaurant.Infrastructure.Data;
+
+namespace FindMyRestaurant.Infrastructure.Repositories
+{
+    public class RestaurantCategoryRepository : Repository<RestaurantCategory>, IRestaurantCategoryRepository
+    {
+        public RestaurantCategoryRepository(AppDbContext context)
+            : base(context)
+        {
+        }
+
+    }
+}

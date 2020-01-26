37fec55e4425e8076eb798fb562ce2a6f1d06629
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 20:16:41 +0100

    Add Migration 202001261912492_AddRestaurantsTable

---
+++ FindMyRestaurant/Infrastructure/Data/Migrations/v1_0_0/202001261912492_AddRestaurantsTable.cs
@@ -0,0 +1,26 @@
+namespace FindMyRestaurant.Infrastructure.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddRestaurantsTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Restaurants",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Restaurants");
+        }
+    }
+}

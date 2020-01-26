18cc46b84cecb16c51aa43587d504075be425e94
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 15:40:41 +0100

    Add new Repository Class that implements the IRepository Interface

---
+++ FindMyRestaurant/Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindMyRestaurant.Infrastructure.Repositories
+{
+    public class Repository
+    {
+    }
+}

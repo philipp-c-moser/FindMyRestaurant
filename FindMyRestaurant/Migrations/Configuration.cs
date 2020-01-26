0bba8423d36dc9827a0e292c4605def2f235fbfb
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-26 15:37:15 +0100

    Enable Migrations for Application

---
+++ FindMyRestaurant/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace FindMyRestaurant.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FindMyRestaurant.Infrastructure.Data.AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FindMyRestaurant.Infrastructure.Data.AppDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}

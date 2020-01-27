bb8e17ad2e6f8eb9a0c5f4fb308c1c488b35c9ab
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-27 22:15:06 +0100

    Add CustomHtmlHelper to use for PageTitleRowrs

--- FindMyRestaurant/Framework/Helpers/CustomHtmlHelper.cs
+++ FindMyRestaurant/Framework/Helpers/CustomHtmlHelper.cs
@@ -1,11 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
+using System.Text;
 using System.Web;
 
 namespace FindMyRestaurant.Framework.Helpers
 {
     public static class CustomHtmlHelper
     {
+
+
+        #region Public Methods
+        public static IHtmlString PageTitleRow(string pageTitle)
+        {
+            var html = new StringBuilder();
+
+            html.Append("<!-- start page title -->");
+            html.Append("<div class=\"row\">");
+            html.Append("<div class=\"col-12\">");
+
+            html.Append("<div class=\"page-title-box\">");
+
+            html.Append("<h4 class=\"page-title\">");
+            html.Append(pageTitle);
+            html.Append("</h4>");
+
+            html.Append("</div>");
+
+            html.Append("</div>");
+            html.Append("</div>");
+            html.Append("<!-- end page title -->");
+
+            return new HtmlString(html.ToString());
+        }
+
+
+        #endregion
+
+
+
     }
 }

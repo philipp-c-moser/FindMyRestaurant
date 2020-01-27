4dbcba23d1e0a8c175aaf807f8dda45cf75ed6a2
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-27 21:56:00 +0100

    Add new VisitController

---
+++ FindMyRestaurant/Controllers/VisitController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+using FindMyRestaurant.Framework.Controllers;
+
+namespace FindMyRestaurant.Controllers
+{
+    public class VisitController : MvcControllerBase
+    {
+        // GET: Visit
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

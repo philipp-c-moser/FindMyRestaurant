3c35ca25254ffeac27bd1e7a356a4b930381862a
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-27 23:32:24 +0100

    Add LegalController with two Actions

---
+++ FindMyRestaurant/Controllers/LegalController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+using FindMyRestaurant.Framework.Controllers;
+
+namespace FindMyRestaurant.Controllers
+{
+    public class LegalController : MvcControllerBase
+    {
+        // GET: Legal
+        public ActionResult Imprint()
+        {
+            return View();
+        }
+
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}

eec0d9a142d82abd1743e34a867f789c2e5bd881
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-31 00:19:23 +0100

    Set up SaveVisitDto

---
+++ FindMyRestaurant/Core/Dto/Visit/SaveVisitDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindMyRestaurant.Core.Dto.Visit
+{
+    public class SaveVisitDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfVisit { get; set; }
+        public Domain.Restaurant Restaurant { get; set; }
+    }
+}

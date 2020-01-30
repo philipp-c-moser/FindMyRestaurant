9a6ad95bd7d6289144cbcc5d868be18493cc9b01
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-31 00:11:11 +0100

    Add new Visit DomainModel

---
+++ FindMyRestaurant/Core/Domain/Visit.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace FindMyRestaurant.Core.Domain
+{
+    public class Visit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfVisit { get; set; }
+        public Restaurant Restaurant { get; set; }
+
+    }
+}

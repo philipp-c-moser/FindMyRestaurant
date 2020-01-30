9fce6af2c0fd0dfa40302c79a332da4c2f80adfd
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-31 00:23:01 +0100

    Implement IVisitRepository in VisitRepository

---
+++ FindMyRestaurant/Infrastructure/Repositories/VisitRepository.cs
@@ -0,0 +1,14 @@
+﻿using FindMyRestaurant.Core.Repositories;
+using FindMyRestaurant.Core.Domain;
+using FindMyRestaurant.Infrastructure.Data;
+
+namespace FindMyRestaurant.Infrastructure.Repositories
+{
+    public class VisitRepository : Repository<Visit>, IVisitRepository
+    {
+        public VisitRepository(AppDbContext context)
+            : base(context)
+        {
+        }
+    }
+}

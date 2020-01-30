e8e9be5266aca78389dcb0c465a801a089702f41
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-01-30 21:33:14 +0100

    Add new RestaurantController

---
+++ FindMyRestaurant/Controllers/RestaurantController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+using FindMyRestaurant.Framework.Controllers;
+
+namespace FindMyRestaurant.Controllers
+{
+    public class RestaurantController : MvcControllerBase
+    {
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

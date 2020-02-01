5bf87830869d79acc0ad68b54a5ed9434eb01fcf
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-02-01 13:26:09 +0100

    Update VisitOverviewDto

---
+++ FindMyRestaurant/Core/Dto/Visit/VisitOverviewDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindMyRestaurant.Core.Dto.Visit
+{
+    public class VisitOverviewDto
+    {
+        public int Id { get; set; }
+        public string VisitName { get; set; }
+        public string RestaurantName { get; set; }
+
+    }
+}

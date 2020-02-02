4b469751aa29999583356c15032fb115a049e9a4
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-02-02 11:04:25 +0100

    Add new Rating Domain Model

---
+++ FindMyRestaurant/Core/Domain/Rating/Rating.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindMyRestaurant.Core.Domain.Rating
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

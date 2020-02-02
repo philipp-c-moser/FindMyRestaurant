14714a2f7a853383e14321ac40749a1e856e5469
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-02-02 11:07:40 +0100

    Add new DomainModels "RatingCriteria" and "RatedRatingCriteria"

---
+++ FindMyRestaurant/Core/Domain/Rating/RatedRatingCriteria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindMyRestaurant.Core.Domain.Rating
+{
+    public class RatedRatingCriteria
+    {
+        public int Id { get; set; }
+        public int Value { get; set; }
+        public string Comment { get; set; }
+        public DateTime RatingDate { get; set; }
+
+        public int RatingId { get; set; }
+        public Rating Rating { get; set; }
+
+        public int RatingCriteriaId { get; set; }
+        public RatingCriteria RatingCriteria { get; set; }
+    }
+}

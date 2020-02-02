d30ecffad35fc48608ed5055148afc8ac4a2682f
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-02-02 10:57:23 +0100

    Add new FindController

---
+++ FindMyRestaurant/Controllers/FindController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindMyRestaurant.Controllers
+{
+    public class FindController : Controller
+    {
+        // GET: Find
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

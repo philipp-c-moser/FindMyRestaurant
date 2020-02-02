c06a27793f9219ebedb67db7b1fffe611e93e454
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2020-02-02 10:58:01 +0100

    Use MvcControllerBase to inherit for Mvc-Controllers

--- FindMyRestaurant/Controllers/FindController.cs
+++ FindMyRestaurant/Controllers/FindController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+
+using FindMyRestaurant.Framework.Controllers;
 
 namespace FindMyRestaurant.Controllers
 {
-    public class FindController : Controller
+    public class FindController : MvcControllerBase
     {
         // GET: Find
         public ActionResult Index()
